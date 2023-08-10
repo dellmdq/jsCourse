067727b5078af1f0c27f281403418712926d9ea8
dellmdq <erikdell@gmail.com>
2023-08-10 08:30:59 -0300

    initial commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+//lectura values and variables
+
+let country = 'Argentina';
+let contintent = 'America';
+let population = '45 millon';
+
+console.log(country);
+console.log(contintent);
+console.log(population);
+
+//lecture data types
+let isIsland =
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 -10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let person = "jonas";
+//constants
+let PI = 3.14155;
+
+//naming variables
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
